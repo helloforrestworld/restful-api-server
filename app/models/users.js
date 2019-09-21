2314fe5f8d6617e0f4a28de3152062b8ade8b261
helloforrestworld@163.com <helloforrestworld@163.com>
2019-09-22 02:08:53 +0800

    feat: 个人资料schema设计

--- app/models/users.js
+++ app/models/users.js
@@ -5,7 +5,31 @@ const { Schema, model } = mongoose
 const userSchema = new Schema({
   __v: { type: Number, select: false },
   name: { type: String, required: true },
-  password: { type: String, required: true, select: false }
+  password: { type: String, required: true, select: false },
+  avatarUrl: { type: String },
+  gender: { type: String, enum: ['male', 'female'], default: 'male' },
+  headline: { type: String },
+  locations: {
+    type: [{
+      type: String
+    }]
+  },
+  business: { type: String },
+  employments: {
+    type: [{
+      company: { type: String },
+      job: { type: String },
+    }]
+  },
+  educations: {
+    type: [{
+      school: { type: String },
+      major: { type: String },
+      diploma: { type: Number, enum: [1, 2, 3, 4, 5] },
+      entranceYear: { type: Number },
+      graduationYear: { type: Number }
+    }]
+  }
 })
 
 module.exports = model('User', userSchema)
